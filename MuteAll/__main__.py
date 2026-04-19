58aec4d5b730290c778336bddedd1f419ba602d2
MarcosA2403 <marcosacostacuzco@gmail.com>
2026-04-18 21:31:13 -0500

    Update __main__.py

--- MuteAll/__main__.py
+++ MuteAll/__main__.py
@@ -1,4 +1,11 @@
+import time
 from MuteAll import bot
 
 if __name__ == "__main__":
-    bot.run()
+    while True:
+        try:
+            bot.run()
+        except Exception as e:
+            print("Error:", e)
+            print("Reiniciando en 5 segundos...")
+            time.sleep(5)
